98739833494bb32d8167291d18b4f3392961d56d
Nicolas Freitas <nicolasandrade.de.freitas@gmail.com>
2024-12-13 20:16:37 -0300

    feat: adiciona teste unitário para exclusão de um usuário

---
+++ api/src/domain/check-in/application/use-cases/user/__tests/delete-user.spec.ts
@@ -0,0 +1,26 @@
+import { makeUser } from 'test/factories/make-user'
+import { InMemoryUsersRepository } from 'test/repositories/in-memory-users-repository'
+import { DeleteUserUseCase } from '../delete-user'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+
+let inMemoryUsersRepository: InMemoryUsersRepository
+let sut: DeleteUserUseCase
+
+describe('Delete User', () => {
+  beforeEach(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository()
+    sut = new DeleteUserUseCase(inMemoryUsersRepository)
+  })
+
+  it('should be able delete a user', async () => {
+    const newUser = makeUser({}, new UniqueEntityId('user-1'))
+
+    await inMemoryUsersRepository.create(newUser)
+
+    await sut.execute({
+      userId: 'user-1',
+    })
+
+    expect(inMemoryUsersRepository.items).toHaveLength(0)
+  })
+})
