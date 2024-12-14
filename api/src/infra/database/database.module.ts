1233681bccc91fd3bd02bc2f19c0f3379027d82e
Nicolas Freitas <nicolasandrade.de.freitas@gmail.com>
2024-12-13 21:26:19 -0300

    feat: adiciona repositórios no módulo do banco de dados

--- api/src/infra/database/database.module.ts
+++ api/src/infra/database/database.module.ts
@@ -1,8 +1,28 @@
 import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma.service'
+import { UsersRepository } from '@/domain/check-in/application/repositories/users-repository'
+import { PrismaUsersRepository } from './prisma/repository/prisma-users-repository'
+import { GymsRepository } from '@/domain/check-in/application/repositories/gyms-repository'
+import { PrismaGymsRepository } from './prisma/repository/prisma-gyms-repository'
+import { CheckInsRepository } from '@/domain/check-in/application/repositories/check-ins-repository'
+import { PrismaCheckInsRepository } from './prisma/repository/prisma-check-ins-repository'
 
 @Module({
-  providers: [PrismaService],
-  exports: [PrismaService],
+  providers: [
+    PrismaService,
+    {
+      provide: UsersRepository,
+      useClass: PrismaUsersRepository,
+    },
+    {
+      provide: GymsRepository,
+      useClass: PrismaGymsRepository,
+    },
+    {
+      provide: CheckInsRepository,
+      useClass: PrismaCheckInsRepository,
+    },
+  ],
+  exports: [PrismaService, UsersRepository, GymsRepository, CheckInsRepository],
 })
 export class DatabaseModule {}
