c60c13d4f6c5acaca982d50d049588635088ad83
Nicolas Freitas <nicolasandrade.de.freitas@gmail.com>
2024-12-14 11:17:09 -0300

    feat: adiciona rota para obter o perfil do usuário autenticado

---
+++ api/src/infra/http/controllers/user/__tests/get-user-profile.controller.e2e-spec.ts
@@ -0,0 +1,41 @@
+import { AppModule } from '@/infra/app.module'
+import { DatabaseModule } from '@/infra/database/database.module'
+import { INestApplication } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { Test } from '@nestjs/testing'
+import request from 'supertest'
+import { UserFactory } from 'test/factories/make-user'
+
+describe('Get Profile (E2E)', () => {
+  let app: INestApplication
+  let userFactory: UserFactory
+  let jwt: JwtService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [UserFactory],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    userFactory = moduleRef.get(UserFactory)
+    jwt = moduleRef.get(JwtService)
+
+    await app.init()
+  })
+
+  test('[GET] /accounts/me', async () => {
+    const user = await userFactory.makePrismaUser()
+
+    const accessToken = jwt.sign({ sub: user.id.toString() })
+
+    const response = await request(app.getHttpServer())
+      .get('/accounts/me')
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send()
+
+    expect(response.statusCode).toBe(200)
+    expect(response.body.user.id).toEqual(user.id.toString())
+  })
+})
--- api/src/infra/http/controllers/user/user.module.ts
+++ api/src/infra/http/controllers/user/user.module.ts
@@ -7,14 +7,22 @@ import { CreateAccountController } from './create-account.controller'
 import { CreateUserUseCase } from '@/domain/check-in/application/use-cases/user/create-user'
 import { DeleteAccountController } from './delete-account.controller'
 import { DeleteUserUseCase } from '@/domain/check-in/application/use-cases/user/delete-user'
+import { GetUserProfileController } from './get-user-profile.controller'
+import { GetUserByIdUseCase } from '@/domain/check-in/application/use-cases/user/get-user-by-id'
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
   controllers: [
     AuthenticateUserController,
     CreateAccountController,
+    GetUserProfileController,
     DeleteAccountController,
   ],
-  providers: [AuthenticateUserUseCase, CreateUserUseCase, DeleteUserUseCase],
+  providers: [
+    AuthenticateUserUseCase,
+    CreateUserUseCase,
+    GetUserByIdUseCase,
+    DeleteUserUseCase,
+  ],
 })
 export class UserModule {}
