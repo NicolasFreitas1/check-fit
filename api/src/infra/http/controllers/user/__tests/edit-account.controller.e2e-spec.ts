466ebd9e6da9763e3182a00351277dccb2ac01b8
Nicolas Freitas <nicolasandrade.de.freitas@gmail.com>
2024-12-14 11:53:11 -0300

    feat: adiciona rota para edição da conta do usuário

---
+++ api/src/infra/http/controllers/user/__tests/edit-account.controller.e2e-spec.ts
@@ -0,0 +1,55 @@
+import { AppModule } from '@/infra/app.module'
+import { DatabaseModule } from '@/infra/database/database.module'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { INestApplication } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { Test } from '@nestjs/testing'
+import request from 'supertest'
+import { UserFactory } from 'test/factories/make-user'
+
+describe('Edit Account (E2E)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+  let userFactory: UserFactory
+  let jwt: JwtService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [UserFactory],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    userFactory = moduleRef.get(UserFactory)
+    prisma = moduleRef.get(PrismaService)
+
+    jwt = moduleRef.get(JwtService)
+
+    await app.init()
+  })
+
+  test('[PUT] /accounts/:userId', async () => {
+    const user = await userFactory.makePrismaUser({ name: 'example name' })
+
+    const accessToken = jwt.sign({ sub: user.id.toString() })
+
+    const response = await request(app.getHttpServer())
+      .put(`/accounts/${user.id.toString()}`)
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send({
+        name: 'example name 02',
+        email: 'example@email.com',
+      })
+
+    expect(response.statusCode).toBe(204)
+
+    const userOnDatabase = await prisma.user.findFirst({
+      where: {
+        name: 'example name 02',
+      },
+    })
+
+    expect(userOnDatabase).toBeTruthy()
+  })
+})
--- api/src/infra/http/controllers/user/user.module.ts
+++ api/src/infra/http/controllers/user/user.module.ts
@@ -9,6 +9,8 @@ import { DeleteAccountController } from './delete-account.controller'
 import { DeleteUserUseCase } from '@/domain/check-in/application/use-cases/user/delete-user'
 import { GetUserProfileController } from './get-user-profile.controller'
 import { GetUserByIdUseCase } from '@/domain/check-in/application/use-cases/user/get-user-by-id'
+import { EditAccountController } from './edit-account.controller'
+import { EditUserUseCase } from '@/domain/check-in/application/use-cases/user/edit-user'
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
@@ -16,12 +18,14 @@ import { GetUserByIdUseCase } from '@/domain/check-in/application/use-cases/user
     AuthenticateUserController,
     CreateAccountController,
     GetUserProfileController,
+    EditAccountController,
     DeleteAccountController,
   ],
   providers: [
     AuthenticateUserUseCase,
     CreateUserUseCase,
     GetUserByIdUseCase,
+    EditUserUseCase,
     DeleteUserUseCase,
   ],
 })
