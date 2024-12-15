d2986c124031fc1dd2e4fa55833ffb4c58db7fa7
Nicolas Freitas <nicolasandrade.de.freitas@gmail.com>
2024-12-14 21:13:45 -0300

    feat: adiciona tela de login e de criação de conta

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,15 @@ import { RouterProvider } from "react-router-dom";
 import { ThemeProvider } from "./components/theme/theme-provider";
 import { router } from "./routes";
 import { Toaster } from "sonner";
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="check-fit-theme">
-      <Toaster richColors />
-      <RouterProvider router={router} />
+      <AuthProvider>
+        <Toaster richColors />
+        <RouterProvider router={router} />
+      </AuthProvider>
     </ThemeProvider>
   );
 }
